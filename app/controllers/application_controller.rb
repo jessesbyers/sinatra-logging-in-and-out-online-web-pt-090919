a8f467eccfc860030feddab5dcc899b98eab0e85
Jesse Byers <jesse.s.byers@gmail.com>
2019-12-06 14:26:47 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,10 +12,20 @@ class ApplicationController < Sinatra::Base
 
   post '/login' do
     @user = User.find_by(username: params[:username], password: params[:password])
+    # If there is a match, set the session to the user's ID,
+    # redirect them to the /account route (using redirect to),
+    # and use ERB to display the user's data on the page.
+    if @user
+          session[:user_id] = @user.id
+          redirect to '/account'
 
+        end
+    # If there is no match, render the error page.
+    erb :error
   end
 
   get '/account' do
+    erb :account
 
   end
 
