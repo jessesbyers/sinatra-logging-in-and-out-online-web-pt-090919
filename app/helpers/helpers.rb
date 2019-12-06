3a1778a0755b17137a3578f1ea7c8f11a5856bf7
Jesse Byers <jesse.s.byers@gmail.com>
2019-12-06 14:46:45 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -5,10 +5,10 @@ class Helpers
   end
 
   def self.is_logged_in?(session)
-    
-
-
-
+    session.include?(session[:user_id])
   end
+
+# return true if the user_id is in the session hash and false if not.
+# The Ruby !! operator will come in handy here.
 
 end
